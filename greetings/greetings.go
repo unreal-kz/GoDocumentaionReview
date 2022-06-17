b7acf4deb68844f0f6710432f97380991f5334ca
unreal-kz <ukibas.b@gmail.com>
2022-06-17 09:21:53 +0300

    Empty Name - error handeling has been implemented.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	msg := fmt.Sprintf("Hello, %v. Welcome!", name)
-	return msg
+	return msg, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/unreal-kz/GoDocumentaionReview/greetings"
 )
 
 func main() {
-	fmt.Println("Testings")
-	msg := greetings.Hello("Baubek")
+
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	msg, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(msg)
 }
